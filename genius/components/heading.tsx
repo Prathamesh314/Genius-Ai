0a915bd3c7cb7ff8e6999c7e711b181c61e93449
Prathamesh314 <124865398+Prathamesh314@users.noreply.github.com>
2023-09-06 00:25:18 +0530

    completed conversation page, setting up open ai

--- genius/components/heading.tsx
+++ genius/components/heading.tsx
@@ -17,15 +17,20 @@ const Heading = ({
     iconColor,
     bgColor
 }: HeadingProps) => {
-  return (
-    <>
-        <div className='px-4 lg:px-8 flex items-center gap-x-3 mb-8'>
-            <div className={cn("p-2 w-fit rounded-md",bgColor)}>
-                <Icon className={cn("w-10 h-10",iconColor)}/>
-            </div>
-        </div>
-    </>
+
     
+  return (
+    <div className='px-4 lg:px-8 flex items-center gap-x-3 mb-8'>
+        <div className={cn("p-2 w-fit rounded-md",bgColor)}>
+            <Icon className={cn("w-10 h-10",iconColor)}/>
+        </div>
+        <div className='text-3xl font-bold'>
+            {title}
+            <p className='text-sm text-muted-foreground'>
+                {description}
+            </p>
+        </div>
+    </div>    
   )
 }
 
